acba264f795de2ac5bb18e4f34c5cc66c4700483
luisa2b <143563326+luisarafaela1B@users.noreply.github.com>
2024-10-25 19:41:13 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Laço Comprido',
+    'O que é Laço Comprido?',
+    'O Laço Comprido é um esporte, praticados por quem gosta de cavalos.'
+)
+
+criaCartao(
+    'Laço Comprido',
+    'Para participar desse esporte precisa ser uma raça de animal em espécifico?',
+    'Não, pode ser de qualquer raça só precisa ser bem domado.'
+)
+
+criaCartao(
+    'Laço Comprido',
+    'É um esporte difícil?',
+    'Não. É um esporte difícil que requer muita dedicação.'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz OI em inglês?',
+    'Oi em ingles é HI (RAI)'
+) 
